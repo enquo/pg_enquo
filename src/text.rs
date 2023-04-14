f6b68f08c6296345c91e2c6dd2497a7469085763
Matt Palmer <matt@enquo.org>
2023-04-14 14:07:09 +1000

    Add support for text length and ordering

--- src/text.rs
+++ src/text.rs
@@ -3,18 +3,38 @@ use pgx::*;
 use serde::{Deserialize, Serialize};
 use std::hash::Hash;
 
+use crate::enquo_ore_32_4;
+
 #[derive(
-    Serialize, Deserialize, Debug, Eq, Hash, PartialEq, PostgresType, PostgresEq, PostgresHash,
+    Serialize,
+    Deserialize,
+    Debug,
+    Eq,
+    Hash,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    PostgresType,
+    PostgresEq,
+    PostgresOrd,
+    PostgresHash,
 )]
 #[allow(non_camel_case_types)]
 pub struct enquo_text(Text);
+
+#[pg_extern]
+fn length(t: enquo_text) -> enquo_ore_32_4 {
+    enquo_ore_32_4(t.0.length().expect(
+        "Cannot extract length from instance of enquo_text that doesn't provide length information",
+    ))
+}
 
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
 mod tests {
     use super::*;
     use crate::test_helpers::*;
-    use enquo_core::Text;
+    use enquo_core::{Text, ORE};
     use serde_json;
 
     fn create_test_table() {
@@ -46,7 +66,7 @@ mod tests {
         .unwrap()
         .into_datum();
 
-        for op in vec!["=", "<>"].iter() {
+        for op in vec!["=", "<>", "<", ">", "<=", ">="].iter() {
             assert!(
                 Spi::get_one_with_args::<bool>(
                     "SELECT COUNT(*) = 1 FROM pg_operator WHERE oprname = $1 AND oprleft = $2 AND oprright = $2",
@@ -87,7 +107,7 @@ mod tests {
             .unwrap();
         }
 
-        let v4 = Text::new_with_unsafe_parts("hello, 4 Enquo!", b"test", &field()).unwrap();
+        let v4 = Text::new_with_unsafe_parts("hello, 4 Enquo!", b"test", &field(), None).unwrap();
         let s4 = serde_json::to_string(&v4).unwrap();
 
         assert_eq!(
@@ -130,12 +150,70 @@ mod tests {
         create_test_table();
         create_test_index();
 
-        let value = Text::new_with_unsafe_parts("huzzah!", b"test", &field()).unwrap();
+        let value = Text::new_with_unsafe_parts("huzzah!", b"test", &field(), None).unwrap();
         let s = serde_json::to_string(&value).unwrap();
         Spi::run(&format!(
             r#"INSERT INTO text_tests VALUES ('{}', '{}')"#,
             0, s
         ))
         .unwrap();
+    }
+
+    #[pg_test]
+    fn length_retrieval() {
+        create_test_table();
+
+        let value = Text::new("Hello, Enquo!", b"test", &field()).unwrap();
+        let s = serde_json::to_string(&value).unwrap();
+
+        Spi::run(&format!(
+            r#"INSERT INTO text_tests VALUES ('hello', '{}')"#,
+            s
+        ))
+        .unwrap();
+
+        let ore_len_str = Spi::get_one::<String>("SELECT length(txt)::text FROM text_tests")
+            .unwrap()
+            .unwrap();
+
+        let actual_len: ORE<8, 16, u32> = serde_json::from_str(&ore_len_str).unwrap();
+        let lengths = Text::query_length(13, &field()).unwrap();
+        let expected_len = lengths.compatible_value(&actual_len).unwrap();
+
+        assert_eq!(expected_len, &actual_len);
+    }
+
+    #[pg_test]
+    fn length_querying() {
+        create_test_table();
+
+        for v in vec!["Hello, Enquo!", "Well hello there, young Enquo!", "ohai!"] {
+            let value = Text::new(v, b"test", &field()).unwrap();
+            let s = serde_json::to_string(&value).unwrap();
+
+            Spi::run(&format!(r#"INSERT INTO text_tests VALUES ('{v}', '{s}')"#)).unwrap();
+        }
+
+        let query_value = Text::query_length(10, &field()).unwrap();
+        let query_str = serde_json::to_string(&query_value).unwrap();
+
+        assert_eq!(
+            2,
+            Spi::get_one_with_args::<i64>(
+                "SELECT COUNT(id) FROM text_tests WHERE length(txt) > $1::enquo_set_ore_32_4",
+                vec![arg(&query_str)]
+            )
+            .unwrap()
+            .unwrap()
+        );
+        assert_eq!(
+            "ohai!",
+            Spi::get_one_with_args::<String>(
+                "SELECT id FROM text_tests WHERE length(txt) <= $1::enquo_set_ore_32_4",
+                vec![arg(&query_str)]
+            )
+            .unwrap()
+            .unwrap()
+        );
     }
 }
